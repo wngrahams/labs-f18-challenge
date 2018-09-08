f5e2bd19a9c4df06bee607f3f33920d4870ac2a9
wngrahams <w.grahamstubbs@gmail.com>
2018-09-07 22:31:00 -0400

    Added functionality to get id from pokemon name

--- app.py
+++ app.py
@@ -22,5 +22,18 @@ def get_pokemon_name(pokemon_id):
         return "There is no pokemon with id " + str(pokemon_id)
 
 
+@app.route('/pokemon/<string:pokemon_name>', methods=['GET'])  # restrict dynamic route to GET to comply with pokeAPI
+def get_pokemon_id(pokemon_name):
+
+    request_url = "https://pokeapi.co/api/v2/pokemon/" + str(pokemon_name)
+    poke_request = requests.get(request_url)  # make a new request from pokeAPI
+    api_output = poke_request.json()  # requests.json() returns json data from the api as a dict
+
+    if 'id' in api_output:  # check if there is a pokemon with the given name
+        return str(pokemon_name) + " has id " + str(api_output['id'])
+    else:
+        return "There is no pokemon named " + str(pokemon_name)
+
+
 if __name__ == '__main__':
     app.run()
